9d8987660de1ddd307587a53110d9203fe515887
HttrckCldHKS <hattrick.claudiuhks@gmail.com>
2015-02-19 21:59:24 +0200

    ooo

--- Source/Source.cpp
+++ Source/Source.cpp
@@ -11,5 +11,5 @@ VOID main(VOID)
 	getchar();
 	
 	
-	
-}
+	return;
+};
---
+++ Source/readFile.cpp
@@ -0,0 +1,19 @@
+
+VOID readFile(VOID)
+{
+	PCHAR random = (PCHAR)malloc(0xFF);
+
+	if (random)
+	{
+		free(random);
+		random = nullptr;
+
+		return;
+	};
+
+	//hh
+	
+	return;
+};
+
+
